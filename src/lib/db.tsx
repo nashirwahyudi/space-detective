7d3297dd98033107c29c3edc7bbb20e1b2804a65
Muarrikh Yazka Ginang Prasidana <37464135+muarrikhyazka@users.noreply.github.com>
2025-07-31 15:58:32 +0800

    Enable SSL connection for Alibaba RDS database

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -6,7 +6,7 @@ export const pool = new Pool({
   database: process.env.NEXT_PUBLIC_DB_DATABASE,
   password: process.env.NEXT_PUBLIC_DB_PASSWORD,
   port: parseInt(process.env.NEXT_PUBLIC_DB_PORT || '5432', 10),
-  // ssl: {
-  //   rejectUnauthorized: false, // Required for RDS with SSL
-  // },
+  ssl: {
+    rejectUnauthorized: false, // Required for RDS with SSL
+  },
 });
